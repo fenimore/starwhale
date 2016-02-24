21437ced405da1949962c302bffebc6ceb867f54
polypmer <exorable.ludos@gmail.com>
2016-02-24 15:55:00 +0100

    init commit

---
+++ desktop/src/com/everythingisreally/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.everythingisreally.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.everythingisreally.StarWhale;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarWhale(), config);
+	}
+}
